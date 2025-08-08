2388eadafdb276f39fafe641985cfe5ad11c3475
Joshua Ackerly <ackerlyj05@gmail.com>
2025-08-08 03:07:13 -0400

    Update to types

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import Main from '@/layouts/main';
 
+
 const Welcome: React.FC = () => {
+    
+    const cdn = import.meta.env.VITE_ASSET_URL;
+    console.log('CDN:', import.meta.env.VITE_ASSET_URL);
+console.log('All ENV:', import.meta.env);
+
+
     return (
         <Main>
             <div style={{ padding: '2rem', textAlign: 'center' }}>
