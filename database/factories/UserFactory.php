043c26726136e215270ac0582195b2ea79503368
Joshua Ackerly <ackerlyj05@gmail.com>
2026-01-11 20:15:22 +0000

    Fix UserFactory fake() function and update .gitignore to ignore storage folder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as FakerFactory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
